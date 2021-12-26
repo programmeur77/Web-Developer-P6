b298c5e4091af1f32a695478886dd5771fe33e6a
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-26 22:31:27 +0100

    Added app.use for user routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/user');
+
 const app = express();
 
 mongoose
@@ -28,5 +30,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
