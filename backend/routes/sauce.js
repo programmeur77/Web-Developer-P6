6d70abe18285df640401d07ba087a892b5fe50e7
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-30 07:35:05 +0100

    Added route for like/dislike feature

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,9 +5,10 @@ const auth = require('./../middleware/auth');
 const sauceCtrl = require('./../controllers/sauce');
 const multer = require('./../middleware/multer-config');
 
-router.post('/', auth, multer, sauceCtrl.postOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/', auth, multer, sauceCtrl.postOneSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
