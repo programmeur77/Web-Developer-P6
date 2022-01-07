bf797050a90961b8f787e65e00874e9ff7630332
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-07 18:45:20 +0100

    Add random string env variable to the jwt token decode function instead of plain visible string

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongoMask = require('mongo-mask');
 
 const User = require('./../models/User');
 
@@ -36,7 +37,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+            token: jwt.sign({ userId: user._id }, process.env.JWT_AUTH_STRING, {
               expiresIn: '24h'
             })
           });
