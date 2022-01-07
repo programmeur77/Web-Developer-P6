1a91d7c01a637eaba23f799f42ef3a1bd58f5bb1
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-07 18:44:17 +0100

    Put JWT authentication string in a process.env variable to keep it secret

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.decode(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.decode(token, process.env.JWT_AUTH_STRING);
     const userId = decodedToken.userId;
     req.auth = { userId };
     if (req.body.userId && req.body.userId !== userId) {
